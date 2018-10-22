e4415970d1d21d05790bfa2fedb10fa65168e7b7
aayush <aayushkothari11@yahoo.com>
2018-10-22 22:07:09 +0530

    fucking worked

--- show/send_data.py
+++ show/send_data.py
@@ -5,20 +5,23 @@ from recombee_api_client.api_client import RecombeeClient
 from recombee_api_client.exceptions import APIException
 from recombee_api_client.api_requests import *
 
-client = RecombeeClient('tvseries', 'Xfp4Upkxyb4DpUC99e2wjU3REiOwcbibJnCYqFq97Pt8uAZhwi2aVD1SPzzgEofM')
+client = RecombeeClient('tvseries', 'hw3cPcSNe1QHai4F3VFDcjDZh0GLWr4uMSxEOO60Ns6I314N8Fch4y4abXfvPFs7')
 
 requests = []
 list_of_ratings = SeriesRating.objects.all()
 for i in range(0,len(list_of_ratings)):
-    name = str(list_of_ratings[i].user)
-    series = str(list_of_ratings[i].series)
+    name = list_of_ratings[i].user.id
+    series = list_of_ratings[i].series.id
     rate = list_of_ratings[i].rating
-    print(name + ' ' + series + ' ' + str(rate))
+    rate = (rate-10)/10
+    print(str(name) + ' ' + str(series) + ' ' + str(rate))
     request = AddRating(name, series, rate ,cascade_create=True)
     requests.append(request)
 
 try:
-	client.send(Batch(requests))
+    print('sending')
+    client.send(Batch(requests))
+    print('Send')
 except APIException as e:
 	print(e)
 except ResponseException as e:
